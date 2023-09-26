8540dd5dc84400ca2e7e8fb427ef62cacfcf50e8
Caio Macedo <caio.macedo.silva@outlook.com>
2023-09-25 22:19:53 -0300

    Quinta versao

--- js/conversao.js
+++ js/conversao.js
@@ -1,25 +1,38 @@
-function calcularTemp(event){
+function calcularTemp(event) {
     const tempInic = document.getElementById('temperaturaInicial').value
     const tempFinal = document.getElementById('temperaturaFinal').value
-    const valor = document.getElementById('valor').value
+    const valor = parseFloat(document.getElementById('valor').value)
+    console.log(tempInic, tempFinal, valor)
 
-     
-    if (tempInic === 'Celsius' && tempFinal === "Fahrenheit" ){
-        temperatura = (valor * 9/5) + 32
-    } if (tempInic === 'Celsius' && tempFinal === 'Kelvin'){
-        temperatura= valor + 273.15
-    } if (tempInic === 'Fahrenheit' && tempFinal === 'Celsius'){
-        temperatura = (valor - 32) * 5/9
-    } if (tempInic === 'Fahrenheit' && tempFinal === 'Kelvin'){
-        temperatura = (valor -32) * 5/9 + 273.15
-    } if (tempInic === 'Kelvin' && tempFinal === 'Celsius'){
+
+    if (tempInic === 'Celsius' && tempFinal === "Fahrenheit") {
+        temperatura = (valor * 9 / 5) + 32
+        
+    }
+    if (tempInic === 'Celsius' && tempFinal === 'Kelvin') {
+        temperatura = valor + 273.15
+        
+    }
+    if (tempInic === 'Fahrenheit' && tempFinal === 'Celsius') {
+        temperatura = (valor - 32) * 5 / 9
+        
+    }
+    if (tempInic === 'Fahrenheit' && tempFinal === 'Kelvin') {
+        temperatura = (valor - 32) * 5 / 9 + 273.15
+        
+    }
+    if (tempInic === 'Kelvin' && tempFinal === 'Celsius') {
         temperatura = valor - 273.15
-    } else (tempInic === 'Kelvin' && tempFinal === 'Fahrenheit')
+        
+    }
+    if (tempInic === 'Kelvin' && tempFinal === 'Fahrenheit') {
         temperatura = (valor - 273.15) * 9 / 5 + 32
+        
+    }
 
 
 
     document.getElementById('resultado').innerHTML =
-    ` A temperatura inicial é de: ${tempInic} e o resultado é ${temperatura}`;
+        ` A temperatura inicial é de: ${tempInic} e o resultado é ${temperatura}`;
 
 }
