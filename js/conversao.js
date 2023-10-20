672ef26388b237761dce3a63601a60c8ad155b46
Caio Macedo <caio.macedo.silva@outlook.com>
2023-10-20 09:05:35 -0300

    Tentei arrumar mas ta foda irmao

--- js/conversao.js
+++ js/conversao.js
@@ -7,30 +7,28 @@ function calcularTemp(event) {
 
     if (tempInic === 'Celsius' && tempFinal === "Fahrenheit") {
         temperatura = (valor * 9 / 5) + 32
-        
+
     }
     if (tempInic === 'Celsius' && tempFinal === 'Kelvin') {
         temperatura = valor + 273.15
-        
+
     }
     if (tempInic === 'Fahrenheit' && tempFinal === 'Celsius') {
         temperatura = (valor - 32) * 5 / 9
-        
+
     }
     if (tempInic === 'Fahrenheit' && tempFinal === 'Kelvin') {
         temperatura = (valor - 32) * 5 / 9 + 273.15
-        
+
     }
     if (tempInic === 'Kelvin' && tempFinal === 'Celsius') {
         temperatura = valor - 273.15
-        
+
     }
     if (tempInic === 'Kelvin' && tempFinal === 'Fahrenheit') {
         temperatura = (valor - 273.15) * 9 / 5 + 32
-        
+
     }
-
-
 
     document.getElementById('resultado').innerHTML =
         ` A temperatura inicial é de: ${tempInic} e o resultado é ${temperatura}`;
